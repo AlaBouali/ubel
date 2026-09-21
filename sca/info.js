cfb163fcb0f2873a0b61de6bc8a5eeea73ffcd4f
AlaBouali <ala.bouali.1997@gmail.com>
2026-09-21 02:48:56 +0200

    new npm pkg version

--- sca/info.js
+++ sca/info.js
@@ -1,4 +1,4 @@
-export const TOOL_VERSION = "0.16.1";
+export const TOOL_VERSION = "0.16.2";
 export const TOOL_NAME = "@arcane-spark/ubel-node";
 export const TOOL_LICENSE = "SEE LICENSE IN LICENSE.md";
 export const AUTHOR = "Ala Bouali";
